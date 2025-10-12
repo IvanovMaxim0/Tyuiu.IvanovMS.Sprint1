d83edbf8b61d2e3677438fdebb2c2b090fdd00e9
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-10-13 01:00:00 +0500

    Task5

---
+++ Tyuiu.IvanovMS.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.IvanovMS.Sprint1.Task5.V7.Lib;
+namespace Tyuiu.IvanovMS.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double f = 360;
+            DataService ds = new DataService();
+            double h = ds.AngleToHoursMinutes(f);
+
+            int result = Convert.ToInt32(h);
+
+            int wait = 12;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint1.Task5.V7/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.IvanovMS.Sprint1.Task5.V7.Lib;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выолнил: Иванов М. C. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Преобразование типов и класс Convert                                    *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнил: Иванов Максим Сергеевич         | ПКТБ-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая решает следующую задачу: Определить h -     *");
+        Console.WriteLine("* полное количество часов прошедших от начала суток до того момента       *");
+        Console.WriteLine("* (в первой половине дня), когда часовая стрелка повернулась на f         *");
+        Console.WriteLine("* градусов (0<f<360, f – вещественное число).                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение f: ");
+        double f = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int h = Convert.ToInt32(ds.AngleToHoursMinutes(f));
+        Console.WriteLine(h); 
+
+        Console.ReadKey();
+    }
+}
