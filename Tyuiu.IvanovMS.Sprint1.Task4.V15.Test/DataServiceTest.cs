9e47afca1dedfb6e88170cd2331ca1362e3512ea
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-10-13 00:11:38 +0500

    Task4

---
+++ Tyuiu.IvanovMS.Sprint1.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.IvanovMS.Sprint1.Task4.V15.Lib;
+namespace Tyuiu.IvanovMS.Sprint1.Task4.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 1;
+            double wait = 0.885;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
