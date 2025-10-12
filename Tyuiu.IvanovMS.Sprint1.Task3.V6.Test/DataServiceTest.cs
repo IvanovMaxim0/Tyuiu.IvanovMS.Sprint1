5e927eae0a7d3fa78d9d23869b667061aafdcdb8
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-10-12 23:25:37 +0500

    Task3

---
+++ Tyuiu.IvanovMS.Sprint1.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.IvanovMS.Sprint1.Task3.V6.Lib;
+namespace Tyuiu.IvanovMS.Sprint1.Task3.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 100;
+            double y = 6;
+            double z = 8;
+            var res = ds.TravelCost(x, y, z);
+            Assert.AreEqual(96, res);
+
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint1.Task3.V6/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.IvanovMS.Sprint1.Task3.V6.Lib;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выолнил: Иванов М. C. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Операторы составного присваивания                                       *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Иванов Максим Сергеевич         | ПКТБ-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу вычисления стоимости поездки на автомобиле           *");
+        Console.WriteLine("* на дачу (туда и обратно).                                               *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        double x = 67;
+        double y = 8.5;
+        double z = 6.5;
+        Console.WriteLine("Расстояние до дачи (км) -> " + x);
+        Console.WriteLine("Расход бензина (литров на 100 км пробега) -> " + y);
+        Console.WriteLine("Цена литра бензина (руб.) ->" + z);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Стоимость поездки: " + ds.TravelCost(x, y, z));
+        Console.ReadKey();
+    }
+}
