805166b005203b73e70ef669b7655e934e26f974
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-10-12 19:40:56 +0500

    Task1

--- Tyuiu.IvanovMS.Sprint1.Task1.V15/Program.cs
+++ Tyuiu.IvanovMS.Sprint1.Task1.V15/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.IvanovMS.Sprint1.Task0.V24.Lib;
+﻿using Tyuiu.IvanovMS.Sprint1.Task1.V15.Lib;
 class Program
 {
     static void Main(string[] args)
@@ -8,24 +8,29 @@ class Program
         Console.Title = "Спринт #0 | Выполнил: Иванов М. С. | ПКТб-25-1";
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #1                                                               *");
-        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-        Console.WriteLine("* задание #0                                                              *");
-        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Тема: Организация ввода вывода в консольных приложениях                 *");
+        Console.WriteLine("* задание #1                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
         Console.WriteLine("* Выполнил: Иванов Максим Сергеевич | ПКТб-25-1                           *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
-        Console.WriteLine("* Написать программу, которая вычисляет выражение 2 * 4 / 4 / 2 + 1       *");
-        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле (4+2*x)/7 и печатает его на экране.      *");
         Console.WriteLine("*                                                                         *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
-        Console.WriteLine("* 2 * 4 / 4 / 2 + 1                                                       *");
+
+        double x;
+
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
 
-        Console.WriteLine(ds.Calculate());
+        Console.WriteLine(ds.Calculate(x));
 
         Console.ReadLine();
     }
